a2a3061860e209169e18ba371b7615f614b388b0
Patrik Österljung <patrik_osterljung@hotmail.com>
2021-09-30 12:16:13 +0200

    feat: change input_integer method to input_integer_range, create input_integer and input_bool methods

--- inputhandler.py
+++ inputhandler.py
@@ -1,5 +1,13 @@
 class InputHandler():
-    def input_integer(message, low, high):
+    """
+    This class acts as a utility class containing static methods 
+    for handling input in other modules.
+    """
+    def input_integer_range(message, low, high):
+        """
+        Method for handling integer input within a desired range,
+        will run until valid input is given
+        """
         while(True):
             try:
                 num = int(input(message))
@@ -10,3 +18,35 @@ class InputHandler():
                     print('Number must be within desired range\n')
             except:
                 print('Only enter a whole number within the desired range, please\n')
+
+
+    def input_integer(message):
+        """
+        Method for handling integer input of any value, 
+        will run until valid input is given
+        """
+        while(True):
+            try:
+                    num = int(input(message))
+                    return num
+                    break
+            except:
+                print('Only enter a whole number, please')
+        
+    
+    def input_bool(message):
+        """
+        Method for handling yes or no input,
+        returns True or False depending on input
+        will run until valid input is given
+        """
+        while(True):
+            value = input(message)
+            if value.lower() == 'y':
+                return True
+                break
+            elif value.lower() == 'n':
+                return False
+                break
+            else:
+                print('please enter value y(es) or n(o)')
