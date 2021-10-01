a583c5a7388096528bec4d74259c6f2bce19a1e1
Patrik Österljung <patrik_osterljung@hotmail.com>
2021-10-01 08:57:17 +0200

    feat: create secret_number method for generating a sequence of random numbers

--- mastermind.py
+++ mastermind.py
@@ -4,11 +4,30 @@ class Mastermind:
     This class contains the logic for the game mastermind, 
     all methods needed to run the game is contained in this class
     """
-    def __init__(self, nrLow = 0, nrHigh = 9, length = 4):
+    def __init__(self, nr_low = 0, nr_high = 9, length = 4):
         """
         The constructor is set up so you can alter the number range 
         and lenth of the secret code for a more varied game
         """
-        self.nrLow = nrLow
-        self.nrHigh = nrHigh
+        self.nr_low = nr_low
+        self.nr_high = nr_high
         self.length = length
+
+
+    def secret_number(self, nr_low, nr_high, length): 
+        """
+        This method will generate a random sequence of numbers 
+        with nrLow being hte lowest and nrHigh being the highest
+        using length. If the difference between highest and lowest
+        is larger than or equal to length, the numbers will be unique. 
+        If not uniqueness is not possible, and is therefore not required 
+        """
+        code = []
+        while len(code) < length:
+            random_number = random.randint(nr_low, nr_high)
+            if nr_high - nr_low >= length:
+                if random_number not in code:
+                    code.append(random_number)
+            else:
+                code.append(random_number)
+        return code
