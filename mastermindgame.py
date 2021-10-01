c9f438dcf1c2ebd88d6383dc60e1085a929858ac
Patrik Österljung <patrik_osterljung@hotmail.com>
2021-10-01 10:42:20 +0200

    feat: create welcome screen method for displaying game information

--- mastermindgame.py
+++ mastermindgame.py
@@ -11,3 +11,27 @@ class RunMastermind:
         self.best_score = 0
         self.worst_score = 0
         self.result_list = []
+
+
+    def welcome_screen(self):
+        """
+        This method runs a welcome screen informing the user of what 
+        this is and how to play it
+        """
+        print('*M*A*S*T*E*R*M*I*N*D*')
+        print('Welcome to Mastermind!')
+        print('The goal of this game is to guess the secret code.\n' + 
+        'You have as many guesses as you need.\n' +
+        'After every guess you will see a result of that guess.\n'+
+        'A result may look like this:\n'+
+        'Your guess: 1234'+
+        '[1, -, C, -]')
+        print('This means the following:\n'+
+        'The first number, 1, is in the correct position\n'+
+        'The second number, 2, is not included in the secret code\n'+
+        'The third number, 3, is in the code but is in the wrong position\n'+
+        'The fourth number, 4, is not included in the code')
+        print('When you have the correct numbers in the right place, you win!\n'+
+        'Try to beat the game in as few guesses as possible.\n'+
+        'The first thing you will do is decide if you want standard or cusom game.\n'+
+        'Only the standard game can save you highscore')
