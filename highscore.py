c27a3e5575bb252625d1913e5507ba9d7731f7d0
Patrik Österljung <patrik_osterljung@hotmail.com>
2021-10-03 13:01:15 +0200

    feat: create Highscore class, constructor and __str__ for it

--- highscore.py
+++ highscore.py
@@ -18,3 +18,23 @@ class HighScoreManager:
         for d in data:
             list.append(Highscore(d[0], int(d[1]), int(d[2]), int(d[3]), float(d[4])))
         return list
+
+
+class Highscore:
+    """
+    This class holds information about a highscore 
+    converted from a worksheet table
+    """
+    def __init__(self, name, item1, item2, item3, item4):
+        self.name = name
+        self.item1 = item1
+        self.item2 = item2
+        self.item3 = item3
+        self.item4 = item4
+    
+
+    def __str__(self):
+        """
+        Sets the default return value when print(highscore) is called
+        """
+        return f'{self.name}\t{self.item1}\t{self.item2}\t{self.item3}\t{self.item4}'
