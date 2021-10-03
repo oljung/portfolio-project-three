03bdeb89c05716dbdc7fa9967325faf473ee2539
Patrik Österljung <patrik_osterljung@hotmail.com>
2021-10-03 19:47:47 +0200

    feat: create sorting methods for highscore manager class

--- highscore.py
+++ highscore.py
@@ -4,9 +4,10 @@ class HighScoreManager:
     """
     Class for handling a list of Highscore instances
     """
-    def __init__(self, data):
+    def __init__(self, data, poker=False):
         self.heading = data.pop(0)
         self.highscore_list = self.create_list(data)
+        self.poker = poker
     
 
     def create_list(self, data):
@@ -18,6 +19,39 @@ class HighScoreManager:
         for d in data:
             list.append(Highscore(d[0], int(d[1]), int(d[2]), int(d[3]), float(d[4])))
         return list
+    
+
+    def sort_list(self, key_value):
+        """
+        Sorts the list of highscore instances using the 
+        key_value as key with operator.attrgetter
+        """
+        self.highscore_list.sort(key = operator.attrgetter(key_value))
+
+
+    def print_heading(self):
+        """
+        Prints the heading formatted as a table
+        """
+        print(f'{self.heading[0]}\t{self.heading[1]}\t{self.heading[2]}\t{self.heading[3]}\t{self.heading[4]}\t')
+    
+    
+    def print_table(self):
+        """
+        Prints header and the items in highscore_list 
+        formatted as a table
+        """
+        self.print_heading()
+        for item in self.highscore_list:
+            print(item)
+
+
+    def show_best_five(self, poker):
+        """
+        Shows the five best averages or win rates with 
+        at least five rounds played
+        """
+        best_five = []
 
 
 class Highscore:
